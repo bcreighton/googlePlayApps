e0e59ede1dc9491a0f6a0009dff5d5fe1624e700
Bryson Creighton <brysoncreighton@gmail.com>
2020-05-28 21:50:59 -0600

    Update Application Component
    Added all necessary details to render in an application block.
    
    Removed appList component as there is no need for the additional component.

--- google-play-apps-client/src/components/application/Application.js
+++ google-play-apps-client/src/components/application/Application.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import './Application.css';
 
 class Application extends Component {
   render() {
     return (
       <div className='application'>
         <h2 className="appTitle">{this.props.title}</h2>
+        <h3 className="appCategory">{this.props.category}</h3>
+        <h3 className="appGenres">{this.props.genres}</h3>
+        <h3 className="appRating">{this.props.rating}</h3>
 
+        <p className="appReviews">{this.props.reviews}</p>
+        <p className="appInstalls">{this.props.installs}</p>
+        <h3 className="appPrice">{this.props.price}</h3>
+        <h4 className="appContentRating">{this.props.contentRating}</h4>
+        <p className="appUpdate">{this.props.update}</p>
+        <p className="appVersion">{this.props.version}</p>
       </div>
     );
   }
